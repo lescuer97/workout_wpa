2c13a40dbb513782f95923c53b73cc28fdcd9bb0
leonardo <leoescuer@protonmail.com>
2023-03-26 22:16:44 +0200

    generator for types

--- front_end/protobuf_gen_type.ts
+++ front_end/protobuf_gen_type.ts
@@ -1,40 +1,68 @@
-const path_to_proto = '../proto' as const;
+function ts_type_from_protobuf_type() {
+  // copy proto dir to deno project
+  let copy_proto = Deno.run({ cmd: ["cp", "-r", "../proto", "."] });
 
-
-// return list of file names
- async function file_names(): Promise<string[]> {
-const fileNames: string[] = [];
-  
-  for await (const dirEntry of Deno.readDir(path_to_proto)) {
-    if (dirEntry.isFile) {
-      fileNames.push(dirEntry.name);
+  copy_proto.status().then((status: Deno.ProcessStatus) => {
+    if (status.success) {
+      console.log("copied the proto dir correctly");
+    } else {
+      throw Error("something went wrong in the copying");
     }
+  });
+
+  try {
+    Deno.readDirSync("generated_types");
+  } catch (_) {
+    Deno.mkdirSync("generated_types");
   }
 
-  return fileNames;
- }
-    
+  // command to run protoc
+  const protoc = Deno.run({
+    cmd: ["protoc", "--ts_out=generated_types", "proto/workouts.proto"],
+  });
 
-function regex_change_to_deno_import() {
-    
-}
+  // wait for success to clear the types
+  protoc.status().then((status: Deno.ProcessStatus) => {
+    if (status.success) {
+      let copy_of_types = Deno.run({
+        cmd: ["cp", "generated_types/proto/workouts.ts", "generated_types/"],
+      });
 
+      Deno.run({ cmd: ["rm", "-r", "generated_types/proto"] });
 
+      copy_of_types.status().then(async (status: Deno.ProcessStatus) => {
+        if (status.success) {
+          const workoutsFile: string = await Deno.readTextFile(
+            "generated_types/workouts.ts",
+          );
 
+          const newWorkoutFile = workoutsFile.replaceAll(
+            "google-protobuf",
+            "npm:google-protobuf",
+          );
 
-function ts_type_from_protobuf_type() {
-    //check if dir doesn't exists else create it create directory if not exists
-    if (!Deno.readDirSync("generated_types")) {
-        Deno.mkdirSync("generated_types");
+          Deno.writeTextFile("generated_types/workouts.ts", newWorkoutFile)
+            .then(
+              () => {
+                console.log("Written to file correctly");
+              },
+            ).catch((err) => {
+              throw Error(err);
+            });
+
+          console.log("copied the types correctly");
+        }
+      });
+    } else {
+      throw Error("something went wrong in the compilation");
     }
-    // command to run protoc
-    Deno.run({ cmd: ["protoc", "--ts_out=generated_types", "../proto/*.proto"] });
-
-
-
+  }).then((res) => {
+    Deno.run({ cmd: ["rm", "-r", "proto"] });
+  });
 }
+
 ts_type_from_protobuf_type();
 
-file_names().then((fileNames) => {
-  console.log(fileNames);
-})
+// file_names().then((fileNames) => {
+//   console.log(fileNames);
+// })
