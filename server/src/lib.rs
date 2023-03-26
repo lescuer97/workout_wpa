d3bf5eeeb7bab0fafb1870c5de2d45da48e58a97
leonardo <leoescuer@protonmail.com>
2023-03-26 13:41:56 +0200

    build system for proto on server

---
+++ server/src/lib.rs
@@ -0,0 +1,32 @@
+// use workouts::workouts_server::{Workouts, WorkoutsServer};
+
+// use workouts::WeightType;
+
+pub mod workouts {
+    include!(concat!(env!("OUT_DIR"), "/workouts.rs"));
+}
+
+impl workouts::Excercise {
+    pub fn default() -> workouts::Excercise {
+        workouts::Excercise {
+            name: "".to_string(),
+            weight: 0,
+            media_url: "".to_string(),
+            sets: 0,
+            rest: 0,
+            weight_unit: workouts::WeightType::Kilograms.into(),
+        }
+    }
+    pub fn new(name: String, weight: i32, media_url: String, sets: i32, rest: i32, weight_type: workouts::WeightType) -> workouts::Excercise {
+        workouts::Excercise {
+            name,
+            weight,
+            media_url,
+            sets,
+            rest,
+            weight_unit: weight_type.into(),
+        }
+    }
+}
+
+
